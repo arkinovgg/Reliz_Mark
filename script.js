dc6c799575f37f9dc5bdb1a1a14157d9b37ace48
arkinovgg <mkaliberda09@gmail.com>
2024-09-27 17:27:17 +0300

    new pages, shop

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const cartBtn = document.getElementById('cartBtn')
 
+cartBtn.addEventListener('click', function () {
+    window.location.assign('cart.html')
+})
+
+const mainSite = document.getElementById('mainSite')
+
+mainSite.addEventListener('click', function () {
+    window.location.assign('index.html')
+})
