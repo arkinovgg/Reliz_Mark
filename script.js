7b2f9be6e723947ca12e92d9063846e15d1865e5
arkinovgg <mkaliberda09@gmail.com>
2024-10-22 15:48:42 +0300

    update

--- script.js
+++ script.js
@@ -10,7 +10,107 @@ cartBtn.addEventListener('click', function () {
 //     window.location.assign('index.html')
 // })
 
-
+// Функція для отримання значення кукі за ім'ям
+function getCookieValue(cookieName) {
+    // Розділяємо всі куки на окремі частини
+    const cookies = document.cookie.split(';');
+  
+    // Шукаємо куки з вказаним ім'ям
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim(); // Видаляємо зайві пробіли
+  
+        // Перевіряємо, чи починається поточне кукі з шуканого імені
+        if (cookie.startsWith(cookieName + '=')) {
+            // Якщо так, повертаємо значення кукі
+            return cookie.substring(cookieName.length + 1); // +1 для пропуску символу "="
+        }
+    }
+    // Якщо кукі з вказаним іменем не знайдено, повертаємо порожній рядок або можна повернути null
+    return '';
+  }
+  
+  
+  // Створення класу кошика
+  class ShoppingCart {
+    constructor() {
+        this.items = {};
+        this.cartCounter = document.querySelector('.cart-counter');// отримуємо лічильник кількості товарів у кошику
+        this.cartElement = document.querySelector('#cart-items'); 
+        this.loadCartFromCookies(); // завантажуємо з кукі-файлів раніше додані в кошик товари
+    }
+  
+    // Додавання товару до кошика
+    addItem(item) {
+        if (this.items[item.title]) {
+            this.items[item.title].quantity += 1; // Якщо товар вже є, збільшуємо його кількість на одиницю
+        } else {
+            this.items[item.title] = item; // Якщо товару немає в кошику, додаємо його
+            this.items[item.title].quantity = 1;
+        }
+        this.updateCounter(); // Оновлюємо лічильник товарів
+        this.saveCartToCookies();
+    }
+  
+    // Зміна кількості товарів товарів
+    updateQuantity(itemTitle, newQuantity) {
+        if (this.items[itemTitle]) {
+            this.items[itemTitle].quantity = newQuantity;
+            if (this.items[itemTitle].quantity == 0) {
+                delete this.items[itemTitle];
+            }
+            this.updateCounter();
+            this.saveCartToCookies();
+        }
+    }
+  
+    // Оновлення лічильника товарів
+    updateCounter() {
+        let count = 0;
+        for (let key in this.items) { // проходимося по всіх ключах об'єкта this.items
+            count += this.items[key].quantity; // рахуємо кількість усіх товарів
+        }
+        this.cartCounter.innerHTML = count; // оновлюємо лічильник на сторінці
+    }
+  
+    // Зберігання кошика в кукі
+    saveCartToCookies() {
+        let cartJSON = JSON.stringify(this.items);
+        document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`;
+    }
+  
+    // Завантаження кошика з кукі
+    loadCartFromCookies() {
+        let cartCookie = getCookieValue('cart');
+        if (cartCookie && cartCookie !== '') {
+            this.items = JSON.parse(cartCookie);
+            this.updateCounter();
+        }
+    }
+    // Обчислення загальної вартості товарів у кошику
+    calculateTotal() {
+        let total = 0;
+        for (let key in this.items) { // проходимося по всіх ключах об'єкта this.items
+            total += this.items[key].price * this.items[key].quantity; // рахуємо вартість усіх товарів
+        }
+        return total;
+    }
+  }
+  
+  // Створення об'єкта кошика 
+  let cart = new ShoppingCart();
+  
+  
+  // Функція для додавання товару до кошика при кліку на кнопку "Купити"
+  function addToCart(event) {
+    // Отримуємо дані про товар з data-атрибута кнопки
+    const productData = event.target.getAttribute('data-product');
+    const product = JSON.parse(productData);
+  
+    // Додаємо товар до кошика
+    cart.addItem(product);
+    console.log(cart);
+  }
+  
 
 // Отримуємо дані про товари з JSON файлу
 async function getProducts() {
@@ -26,7 +126,7 @@ function getCardHTML(product) {
 
     return `
     <div class="card">
-          <div class="card-img" style="background-image: url('${product.image}'); background-position: '${product.backposy}'% '${product.backposx}'% "></div>
+          <div class="card-img" style="background-image: url('${product.image}'); background-position: ${product.backposy}% ${product.backposx}%"></div>
           <div class="card-info">
               <p class="text-title">${product.title}</p>
               <p class="text-body2">${product.desc}</p>
@@ -34,7 +134,7 @@ function getCardHTML(product) {
           <div class="card-footer">
               <span class="text-title">${product.price} ₴</span>
               <button type="button" class="card-button cart-btn" data-product='${productData}'>
-                  <svg class="svg-icon" viewBox="0 0 20 20">
+                  <svg class="svg-icon" viewBox="0 0 20 20" data-product='${productData}'>
                       <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
                       <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
                       <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
@@ -80,7 +180,7 @@ function getCardHTML2(product) {
 
     return `
     <div class="card2">
-          <div class="card-img" style="background-image: url('${product.image}'); background-position: '${product.backposy}'% '${product.backposx}'% "></div>
+          <div class="card-img" style="background-image: url('${product.image}'); background-position: ${product.backposy}% ${product.backposx}%"></div>
           <div class="card-info">
               <p class="text-title">${product.title}</p>
               <p class="text-body2">${product.desc}</p>
@@ -88,7 +188,7 @@ function getCardHTML2(product) {
           <div class="card-footer">
               <span class="text-title">${product.price} ₴</span>
               <button type="button" class="card-button cart-btn" data-product='${productData}'>
-                  <svg class="svg-icon" viewBox="0 0 20 20">
+                  <svg class="svg-icon" viewBox="0 0 20 20" data-product='${productData}'>
                       <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
                       <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
                       <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
@@ -132,7 +232,7 @@ function getCardHTML3(product) {
 
     return `
     <div class="card2">
-          <div class="card-img" style="background-image: url('${product.image}')"></div>
+          <div class="card-img" style="background-image: url('${product.image}'); background-position: ${product.backposy}% ${product.backposx}%""></div>
           <div class="card-info">
               <p class="text-title">${product.title}</p>
               <p class="text-body2">${product.desc}</p>
@@ -140,7 +240,7 @@ function getCardHTML3(product) {
           <div class="card-footer">
                <span class="text-title1">${product.price} ₴</span><span class="text-title2">${product.new_price} ₴</span>
               <button type="button" class="card-button cart-btn" data-product='${productData}'>
-                  <svg class="svg-icon" viewBox="0 0 20 20">
+                  <svg class="svg-icon" viewBox="0 0 20 20" data-product='${productData}'>
                       <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
                       <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
                       <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
@@ -192,7 +292,7 @@ function getCardHTML4(product) {
           <div class="card-footer">
               <span class="text-title">${product.price} ₴</span>
               <button type="button" class="card-button cart-btn" data-product='${productData}'>
-                  <svg class="svg-icon" viewBox="0 0 20 20">
+                  <svg class="svg-icon" viewBox="0 0 20 20" data-product='${productData}'>
                       <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
                       <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
                       <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
@@ -243,7 +343,7 @@ function getCardHTML5(product) {
           <div class="card-footer">
               <span class="text-title">${product.price} ₴</span>
               <button type="button"  class="card-button cart-btn" data-product='${productData}'>
-                  <svg class="svg-icon" viewBox="0 0 20 20">
+                  <svg class="svg-icon" viewBox="0 0 20 20" data-product='${productData}'>
                       <path d="M17.72,5.011H8.026c-0.271,0-0.49,0.219-0.49,0.489c0,0.271,0.219,0.489,0.49,0.489h8.962l-1.979,4.773H6.763L4.935,5.343C4.926,5.316,4.897,5.309,4.884,5.286c-0.011-0.024,0-0.051-0.017-0.074C4.833,5.166,4.025,4.081,2.33,3.908C2.068,3.883,1.822,4.075,1.795,4.344C1.767,4.612,1.962,4.853,2.231,4.88c1.143,0.118,1.703,0.738,1.808,0.866l1.91,5.661c0.066,0.199,0.252,0.333,0.463,0.333h8.924c0.116,0,0.22-0.053,0.308-0.128c0.027-0.023,0.042-0.048,0.063-0.076c0.026-0.034,0.063-0.058,0.08-0.099l2.384-5.75c0.062-0.151,0.046-0.323-0.045-0.458C18.036,5.092,17.883,5.011,17.72,5.011z"></path>
                       <path d="M8.251,12.386c-1.023,0-1.856,0.834-1.856,1.856s0.833,1.853,1.856,1.853c1.021,0,1.853-0.83,1.853-1.853S9.273,12.386,8.251,12.386z M8.251,15.116c-0.484,0-0.877-0.393-0.877-0.874c0-0.484,0.394-0.878,0.877-0.878c0.482,0,0.875,0.394,0.875,0.878C9.126,14.724,8.733,15.116,8.251,15.116z"></path>
                       <path d="M13.972,12.386c-1.022,0-1.855,0.834-1.855,1.856s0.833,1.853,1.855,1.853s1.854-0.83,1.854-1.853S14.994,12.386,13.972,12.386z M13.972,15.116c-0.484,0-0.878-0.393-0.878-0.874c0-0.484,0.394-0.878,0.878-0.878c0.482,0,0.875,0.394,0.875,0.878C14.847,14.724,14.454,15.116,13.972,15.116z"></path>
@@ -327,106 +427,3 @@ function searchProducts(event) {
 
 let searchForm = document.querySelector('#searchForm');
 searchForm.addEventListener('submit', searchProducts);
-
-
-
-// Функція для отримання значення кукі за ім'ям
-function getCookieValue(cookieName) {
-  // Розділяємо всі куки на окремі частини
-  const cookies = document.cookie.split(';');
-
-  // Шукаємо куки з вказаним ім'ям
-  for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim(); // Видаляємо зайві пробіли
-
-      // Перевіряємо, чи починається поточне кукі з шуканого імені
-      if (cookie.startsWith(cookieName + '=')) {
-          // Якщо так, повертаємо значення кукі
-          return cookie.substring(cookieName.length + 1); // +1 для пропуску символу "="
-      }
-  }
-  // Якщо кукі з вказаним іменем не знайдено, повертаємо порожній рядок або можна повернути null
-  return '';
-}
-
-
-// Створення класу кошика
-class ShoppingCart {
-  constructor() {
-      this.items = {};
-      this.cartCounter = document.querySelector('.cart-counter');// отримуємо лічильник кількості товарів у кошику
-      this.cartElement = document.querySelector('#cart-items'); 
-      this.loadCartFromCookies(); // завантажуємо з кукі-файлів раніше додані в кошик товари
-  }
-
-  // Додавання товару до кошика
-  addItem(item) {
-      if (this.items[item.title]) {
-          this.items[item.title].quantity += 1; // Якщо товар вже є, збільшуємо його кількість на одиницю
-      } else {
-          this.items[item.title] = item; // Якщо товару немає в кошику, додаємо його
-          this.items[item.title].quantity = 1;
-      }
-      this.updateCounter(); // Оновлюємо лічильник товарів
-      this.saveCartToCookies();
-  }
-
-  // Зміна кількості товарів товарів
-  updateQuantity(itemTitle, newQuantity) {
-      if (this.items[itemTitle]) {
-          this.items[itemTitle].quantity = newQuantity;
-          if (this.items[itemTitle].quantity == 0) {
-              delete this.items[itemTitle];
-          }
-          this.updateCounter();
-          this.saveCartToCookies();
-      }
-  }
-
-  // Оновлення лічильника товарів
-  updateCounter() {
-      let count = 0;
-      for (let key in this.items) { // проходимося по всіх ключах об'єкта this.items
-          count += this.items[key].quantity; // рахуємо кількість усіх товарів
-      }
-      this.cartCounter.innerHTML = count; // оновлюємо лічильник на сторінці
-  }
-
-  // Зберігання кошика в кукі
-  saveCartToCookies() {
-      let cartJSON = JSON.stringify(this.items);
-      document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`;
-  }
-
-  // Завантаження кошика з кукі
-  loadCartFromCookies() {
-      let cartCookie = getCookieValue('cart');
-      if (cartCookie && cartCookie !== '') {
-          this.items = JSON.parse(cartCookie);
-          this.updateCounter();
-      }
-  }
-  // Обчислення загальної вартості товарів у кошику
-  calculateTotal() {
-      let total = 0;
-      for (let key in this.items) { // проходимося по всіх ключах об'єкта this.items
-          total += this.items[key].price * this.items[key].quantity; // рахуємо вартість усіх товарів
-      }
-      return total;
-  }
-}
-
-// Створення об'єкта кошика 
-let cart = new ShoppingCart();
-
-
-// Функція для додавання товару до кошика при кліку на кнопку "Купити"
-function addToCart(event) {
-  // Отримуємо дані про товар з data-атрибута кнопки
-  const productData = event.target.getAttribute('data-product');
-  const product = JSON.parse(productData);
-
-  // Додаємо товар до кошика
-  cart.addItem(product);
-  console.log(cart);
-}
